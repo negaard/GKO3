9ef676ea018223a1b6c4f06575e0d374e8725b61
negaard <luknegaard@gmail.com>
2020-10-16 11:07:28 +0200

    server

---
+++ Server/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var http = require("http");
+
+server.listen(port, () => {
+    console.log(`Server-applikation lytter på http://localhost:${port}`)
+  })
